868b9a1bfd349522888133daa25e81fabe0399dd
Garth Bushell <garth.bushell@oracle.com>
2018-02-07 14:42:40 +0000

    Make config consistant across oci

--- pkg/oci/client/config.go
+++ pkg/oci/client/config.go
@@ -31,7 +31,7 @@ type AuthConfig struct {
 	PrivateKey           string `yaml:"key"`
 	Fingerprint          string `yaml:"fingerprint"`
 	Region               string `yaml:"region"`
-	PrivateKeyPassphrase string `yaml:"privateKeyPassphrase"`
+	PrivateKeyPassphrase string `yaml:"key_passphase"`
 }
 
 // Config holds the OCI cloud-provider config passed to Kubernetes compontents.
