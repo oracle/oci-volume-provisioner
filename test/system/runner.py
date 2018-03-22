1bd73259def0fe19032aa497fc0e2529222852ce
timothy_langford <timothy.langford@oracle.com>
2018-03-22 16:40:30 +0000

    Added filesystem itest. Currently is commented while we wait for the flex_volume driver PR to go through. Also, some refactoring to make things easier to read and more consistent.

--- test/system/runner.py
+++ test/system/runner.py
@@ -121,6 +121,7 @@ def _poll(stdout, stderr):
                     stderrbuf_line = _process_stream(stream, read_fds, stderrbuf, stderrbuf_line)
     return (''.join(stdoutbuf), ''.join(stderrbuf))
 
+
 def _run_command(cmd, cwd, display_errors=True):
     _log(cwd + ": " + cmd)
     process = subprocess.Popen(cmd,
@@ -150,63 +151,6 @@ def _kubectl(action, exit_on_error=True, display_errors=True, log_stdout=True):
     return stdout
 
 
-def _get_pod_infos():
-    stdout = _kubectl("-n kube-system get pods -o wide")
-    infos = []
-    for line in stdout.split("\n"):
-        line_array = line.split()
-        if len(line_array) > 0:
-            name = line_array[0]
-            if name.startswith('oci-volume-provisioner'):
-                status = line_array[2]
-                node = line_array[6]
-                infos.append((name, status, node))
-    return infos
-
-
-def _wait_for_pod_status(desired_status):
-    infos = _get_pod_infos()
-    num_polls = 0
-    while not any(i[1] == desired_status for i in infos):
-        for i in infos:
-            _log("    - pod: " + i[0] + ", status: " + i[1] + ", node: " + i[2])
-        time.sleep(1)
-        num_polls += 1
-        if num_polls == TIMEOUT:
-            for i in infos:
-                _log("Error: Pod: " + i[0] + " " + \
-                     "failed to achieve status: " + desired_status + "." + \
-                     "Final status was: " + i[1])
-            sys.exit(1)
-        infos = _get_pod_infos()
-    return (infos[0][0], infos[0][1], infos[0][2])
-
-
-def _get_volume(volume_name):
-    stdout = _kubectl("get PersistentVolumeClaim -o wide")
-    for line in stdout.split("\n"):
-        line_array = line.split()
-        if len(line_array) >= 3:
-            name = line_array[0]
-            status = line_array[1]
-            if name == volume_name and status == "Bound":
-                return line_array[2]
-    return None
-
-
-def _get_volume_and_wait(volume_name):
-    num_polls = 0
-    volume = _get_volume(volume_name)
-    while not volume:
-        _log("    waiting...")
-        time.sleep(1)
-        num_polls += 1
-        if num_polls == TIMEOUT:
-            return False
-        volume = _get_volume(volume_name)
-    return volume
-
-
 def _get_json_doc(response):
     decoder = json.JSONDecoder()
     try:
@@ -234,25 +178,6 @@ def _oci_config():
             sys.exit(1)
 
 
-def _volume_exists(compartment_id, volume, state):
-    client = oci.core.blockstorage_client.BlockstorageClient(_oci_config())
-    volumes = client.list_volumes(compartment_id)
-    for vol in _get_json_doc(str(volumes.data)):
-        if vol['id'].endswith(volume) and vol['lifecycle_state'] == state:
-            return True
-    return False
-
-
-def _wait_for_volume(compartment_id, volume):
-    num_polls = 0
-    while not _volume_exists(compartment_id, volume, 'AVAILABLE'):
-        _log("    waiting...")
-        time.sleep(1)
-        num_polls += 1
-        if num_polls == TIMEOUT:
-            return False
-    return True
-
 def _get_compartment_id(pod_name):
     """
     Gets the oci compartment_id from the oci-volume-provisioner pod host.
@@ -263,6 +188,191 @@ def _get_compartment_id(pod_name):
     result_json = _get_json_doc(str(result))
     compartment_id = result_json["compartmentId"]
     return compartment_id
+
+
+def get_availability_domain(compartment_id, ad_region_key):
+    """
+    Take the specified availability domain region key e.g: 'PHX-AD-1' and 
+    return the tenancy availability domain region key e.g: 'Uocm:PHX-AD-1'
+
+    See: https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/regions.htm?Highlight=list%20availability%20domains
+    """
+    client = oci.identity.identity_client.IdentityClient(_oci_config())
+    ads = client.list_availability_domains(compartment_id)
+    for ad in _get_json_doc(str(ads.data)):
+        if ad['name'].endswith(ad_region_key):
+            return ad['name']
+    _log("Failed to determine the required tenancy for '" + ad_region_key + "' in compartment: '" + compartment_id + "'.")
+    sys.exit(1)
+
+
+def deref(d, *keys):
+    for k in keys:
+        try:
+            d = d[k]
+        except KeyError:
+            return None
+    return d
+
+
+def _extract_pvc_manifest_opts(pvc_manifest_file):
+    with open(pvc_manifest_file, 'r') as stream:
+        try:
+            pvc_manifest = yaml.load(stream)
+            pvc_name = deref(pvc_manifest, 'metadata', 'name')
+            ad_region_key = deref(pvc_manifest, 'spec', 'selector', 'matchLabels', 'oci-availability-domain')
+            return pvc_name, ad_region_key
+        except yaml.YAMLError:
+            _log("Error. Failed to parse pvc manifest file " + pvc_manifest_file)
+            sys.exit(1)
+
+
+def _get_oci_volume_provisioner_infos():
+    stdout = _kubectl("-n kube-system get pods -o wide")
+    infos = []
+    for line in stdout.split("\n"):
+        line_array = line.split()
+        if len(line_array) > 0:
+            name = line_array[0]
+            if name.startswith('oci-volume-provisioner'):
+                status = line_array[2]
+                node = line_array[6]
+                infos.append((name, status, node))
+    return infos
+
+
+def _wait_for_oci_volume_provisioner_status(desired_status):
+    infos = _get_oci_volume_provisioner_infos()
+    num_polls = 0
+    while not any(i[1] == desired_status for i in infos):
+        for i in infos:
+            _log("    - pod: " + i[0] + ", status: " + i[1] + ", node: " + i[2])
+        time.sleep(1)
+        num_polls += 1
+        if num_polls == TIMEOUT:
+            for i in infos:
+                _log("Error: Pod: " + i[0] + " " + \
+                     "failed to achieve status: " + desired_status + "." + \
+                     "Final status was: " + i[1])
+            sys.exit(1)
+        infos = _get_oci_volume_provisioner_infos()
+    return (infos[0][0], infos[0][1], infos[0][2])
+
+
+def get_bound_pvc(pvc_name):
+    stdout = _kubectl("get PersistentVolumeClaim -o wide")
+    for line in stdout.split("\n"):
+        line_array = line.split()
+        if len(line_array) >= 3:
+            name = line_array[0]
+            status = line_array[1]
+            if name == pvc_name and status == "Bound":
+                return line_array[2]
+    return None
+
+
+def _wait_for_bound_pvc(pvc_name):
+    num_polls = 0
+    pvc = get_bound_pvc(pvc_name)
+    while not pvc:
+        _log("    waiting...")
+        time.sleep(1)
+        num_polls += 1
+        if num_polls == TIMEOUT:
+            return False
+        pvc = get_bound_pvc(pvc_name)
+    return pvc
+
+
+def _volume_exists(compartment_id, volume_id, state):
+    client = oci.core.blockstorage_client.BlockstorageClient(_oci_config())
+    volumes = client.list_volumes(compartment_id)
+    for vol in _get_json_doc(str(volumes.data)):
+        if vol['id'].endswith(volume_id) and vol['lifecycle_state'] == state:
+            return True
+    return False
+
+
+def _wait_for_volume(compartment_id, volume_id, state):
+    num_polls = 0
+    while not _volume_exists(compartment_id, volume_id, state):
+        _log("    waiting...")
+        time.sleep(1)
+        num_polls += 1
+        if num_polls == TIMEOUT:
+            return False
+    return True
+
+
+def _test_create_volume(compartment_id, pvc_manifest_file):
+    pvc_name, _ = _extract_pvc_manifest_opts(pvc_manifest_file)
+
+    _log("Creating the volume claim")
+    _kubectl("create -f " + pvc_manifest_file, exit_on_error=False)
+
+    volume_id = _wait_for_bound_pvc(pvc_name)
+    _log("Created volume with ocid: " + volume_id)
+
+    _log("Querying the OCI api to make sure a volume with this name exists...")
+    if not _wait_for_volume(compartment_id, volume_id, 'AVAILABLE'):
+        _log("Failed to find volume with ocid: " + volume_id)
+        sys.exit(1)
+    _log("Volume: " + volume_id + " is present and available")
+
+    _log("Delete the volume claim")
+    _kubectl("delete -f " + pvc_manifest_file, exit_on_error=False)
+
+    _log("Querying the OCI api to make sure a volume with this name now doesnt exist...")
+    if not _wait_for_volume(compartment_id, volume_id, 'TERMINATED'):
+        _log("Failed to terminate volume with ocid: " + volume_id)
+        sys.exit(1)
+    _log("Volume: " + volume_id + " has now been terminated")
+
+
+def _filesystem_exists(compartment_id, filesystem_id, availability_domain, state):
+    client = oci.file_storage.file_storage_client.FileStorageClient(_oci_config())
+    filesystems = client.list_file_systems(compartment_id, availability_domain)
+    for fs in _get_json_doc(str(filesystems.data)):
+        if fs['id'].endswith(filesystem_id) and fs['lifecycle_state'] == state:
+            return True
+    return False
+
+
+def _wait_for_filesystem(compartment_id, filesystem_id, availability_domain, state):
+    num_polls = 0
+    while not _filesystem_exists(compartment_id, filesystem_id, availability_domain, state):
+        _log("    waiting...")
+        time.sleep(1)
+        num_polls += 1
+        if num_polls == TIMEOUT:
+            return False
+    return True
+
+
+def _test_create_filesystem(compartment_id, pvc_manifest_file):
+    pvc_name, ad_region_key = _extract_pvc_manifest_opts(pvc_manifest_file)
+    availability_domain = get_availability_domain(compartment_id, ad_region_key)
+
+    _log("Creating the filesystem claim")
+    _kubectl("create -f " + pvc_manifest_file, exit_on_error=False)
+
+    fs_ocid = _wait_for_bound_pvc(pvc_name)
+    _log("Created filesystem with ocid: " + fs_ocid)
+
+    _log("Querying the OCI api to make sure a filesystem with this name exists...")
+    if not _wait_for_filesystem(compartment_id, fs_ocid, availability_domain, 'ACTIVE'):
+        _log("Failed to find filesystem with ocid: " + fs_ocid)
+        sys.exit(1)
+    _log("Filesystem: " + fs_ocid + " is present and available")
+
+    _log("Delete the volume claim")
+    _kubectl("delete -f " + pvc_manifest_file, exit_on_error=False)
+
+    _log("Querying the OCI api to make sure a filesystem with this name now doesnt exist...")
+    if not _wait_for_filesystem(compartment_id, fs_ocid, availability_domain, 'DELETED'):
+        _log("Failed to delete filesystem with ocid: " + fs_ocid)
+        sys.exit(1)
+    _log("Filesystem: " + fs_ocid + " has now been deleted")
 
 
 def _handle_args():
@@ -299,30 +409,6 @@ def _cleanup(exit_on_error=False, display_errors=True):
              exit_on_error, display_errors)
 
 
-def _test_create_volume(compartment_id, claim_target, claim_volume_name):
-
-    _log("Creating the volume claim")
-    _kubectl("create -f " + claim_target, exit_on_error=False)
-
-    volume = _get_volume_and_wait(claim_volume_name)
-    _log("Created volume with name: " + volume)
-
-    _log("Querying the OCI api to make sure a volume with this name exists...")
-    if not _wait_for_volume(compartment_id, volume):
-        _log("Failed to find volume with name: " + volume)
-        sys.exit(1)
-    _log("Volume: " + volume + " is present and available")
-
-    _log("Delete the volume claim")
-    _kubectl("delete -f " + claim_target, exit_on_error=False)
-
-    _log("Querying the OCI api to make sure a volume with this name now doesnt exist...")
-    if not _volume_exists(compartment_id, volume, 'TERMINATED'):
-        _log("Volume with name: " + volume + " still exists")
-        sys.exit(1)
-    _log("Volume: " + volume + " has now been terminated")
-
-
 def _main():
     _reset_debug_file()
     args = _handle_args()
@@ -353,24 +439,24 @@ def _main():
         _kubectl("create -f ../../dist/oci-volume-provisioner-rbac.yaml", exit_on_error=False)
         _kubectl("create -f ../../dist/oci-volume-provisioner.yaml", exit_on_error=False)
 
-        pod_name, _, _ = _wait_for_pod_status("Running")
-
-    # get the compartment_id of the oci-volume-provisioner
+    pod_name, _, _ = _wait_for_oci_volume_provisioner_status("Running")
     compartment_id = _get_compartment_id(pod_name)
 
     if not args['no_test']:
-        _log("Running system test: Simple", as_banner=True)
-        _test_create_volume(compartment_id,
-                            "../../manifests/example-claim.yaml", "demooci")
+        _log("Running system test: Block volume - Default", as_banner=True)
+        _test_create_volume(compartment_id, "../../manifests/example-claim.yaml")
 
-        _log("Running system test: Ext3 file system", as_banner=True)
-        _test_create_volume(compartment_id,
-                            "../../manifests/example-claim-ext3.yaml", "demo-oci-ext3")
+        _log("Running system test: Block volume - Ext3 file system", as_banner=True)
+        _test_create_volume(compartment_id, "../../manifests/example-claim-ext3.yaml")
 
-        _log("Running system test: No AD specified", as_banner=True)
-        _test_create_volume(compartment_id,
-                            "../../manifests/example-claim-no-AD.yaml", "demooci-no-ad")
-
+        _log("Running system test: Block volume - No AD specified", as_banner=True)
+        _test_create_volume(compartment_id, "../../manifests/example-claim-no-AD.yaml")
+        
+        # trjl - disabled until the new flex_volume_driver (with the fs stuff) is ready!
+        # 
+        # _log("Running system test: NFSv3 filesystem - Default", as_banner=True)
+        # _test_create_filesystem(compartment_id, "../../manifests/example-claim-ffsw.yaml")
+        
     if not args['no_teardown']:
         _log("Tearing down the volume provisioner", as_banner=True)
         _cleanup()
