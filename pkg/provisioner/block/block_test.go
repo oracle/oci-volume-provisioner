2a089561417584a765c9aced63d7bf6cf5d39188
MadalinaPatrichi <ioana-madalina.patrichi@oracle.com>
2018-04-24 14:51:53 +0100

    [WIP] Modified annotiations & unit test
    -Modified backup id to be associated to annotations rathen than the label.
    -Added unit test to partially verify functionality

--- pkg/provisioner/block/block_test.go
+++ pkg/provisioner/block/block_test.go
@@ -15,10 +15,38 @@
 package block
 
 import (
+	"encoding/json"
+	"fmt"
+	"oci-volume-provisioner/pkg/oci/client"
+	"oci-volume-provisioner/pkg/utils"
+	"os"
 	"testing"
 
 	"github.com/kubernetes-incubator/external-storage/lib/controller"
+	"github.com/oracle/oci-go-sdk/core"
+	"github.com/oracle/oci-go-sdk/identity"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/pkg/api/v1"
 )
+
+// LoadConfig loads confuration data from a given path
+func loadConfig(configFilePath string, t *testing.T) *client.ProvisionerClient {
+	f, err := os.Open(configFilePath)
+	if err != nil {
+		t.Fatalf("Unable to load volume provisioner configuration file: %v", configFilePath)
+	}
+	defer f.Close()
+
+	cfg, err := client.LoadConfig(f)
+	if err != nil {
+		t.Fatalf("Unable to load volume provisioner client: %v", err)
+	}
+	pc, err := client.FromConfig(cfg)
+	if err != nil {
+		t.Fatalf("Unable to load volume provisioner client details: %v", err)
+	}
+	return &pc
+}
 
 func TestResolveFSTypeWhenNotConfigured(t *testing.T) {
 	options := controller.VolumeOptions{Parameters: make(map[string]string)}
@@ -35,5 +63,41 @@ func TestResolveFSTypeWhenConfigured(t *testing.T) {
 	fst := resolveFSType(options)
 	if fst != "ext3" {
 		t.Fatalf("Unexpected filesystem type: '%s'.", fst)
+	}
+}
+
+func TestCreateVolumeFromBackup(t *testing.T) {
+	// test creating a volume from an existing backup
+	options := controller.VolumeOptions{PVName: "dummyVolumeOptions"}
+	volumeBackupID := "dummyVolumeBackupId"
+	pv := &v1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				ociVolumeBackupID: "dummy",
+			},
+		},
+	}
+	options.PVC = pv
+	adName := "dummyAdName"
+	adCompartmentID := "dummyCompartmentId"
+	availabilityDomain := identity.AvailabilityDomain{Name: &adName, CompartmentId: &adCompartmentID}
+	configFilePath := "config/blockProvision.yaml"
+	pc := loadConfig(configFilePath, t)
+	block := blockProvisioner{client: *pc}
+	volID := "dummyVolumeId"
+	_createVolumeResp := &core.CreateVolumeResponse{Volume: core.Volume{Id: &volID}}
+	byteResp, _ := json.Marshal(_createVolumeResp)
+	resp := string(byteResp)
+	m := map[string]*string{fmt.Sprintf("/%s/volumes", block.client.BlockStorage().BasePath): &resp}
+	server := testutils.OCIResponseStub(m)
+	defer server.Close()
+	block.client.BlockStorage().Host = server.URL
+	provisionedVolume, err := block.Provision(options, &availabilityDomain)
+	if err != nil {
+		t.Fatalf("Failed to provision volume from block storage: %v", err)
+	}
+	if provisionedVolume.Annotations[ociVolumeID] != volID {
+		t.Fatalf("Failed to assign the id of the blockID: %s, assigned %s instead", volumeBackupID,
+			provisionedVolume.Annotations[ociVolumeID])
 	}
 }
