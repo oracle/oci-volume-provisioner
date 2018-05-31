5e8e9701a857818f49bad1b1824743c492bae519
MadalinaPatrichi <ioana-madalina.patrichi@oracle.com>
2018-05-31 12:21:02 +0100

    TEst ID fix

--- test/system/runner.py
+++ test/system/runner.py
@@ -191,7 +191,7 @@ def _terraform(action, cwd, terraform_env):
     if "KUBECONFIG" not in os.environ and "KUBECONFIG_VAR" not in os.environ:
         (stdout, _, returncode) = _run_command(terraform_env + " terraform " + action, cwd)
     else:
-        (stdout, _, returncode) = _run_command("KUBECONFIG=" + _get_kubeconfig() + terraform_env + " terraform " + action, cwd)
+        (stdout, _, returncode) = _run_command("KUBECONFIG=" + _get_kubeconfig()+ " " + terraform_env + " terraform " + action, cwd)
     if returncode != 0:
         _log("Error running terraform")
         sys.exit(1)
